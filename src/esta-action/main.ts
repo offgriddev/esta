0d4f0cad6dfa0f1d2b91c4aae55b20d38462eb75
Jesse Anderson <jeryanders@gmail.com>
2023-04-21 22:25:02 -0400

    feat: beta dist

--- src/esta-action/main.ts
+++ src/esta-action/main.ts
@@ -1,14 +1,12 @@
 import * as core from '@actions/core'
-import ts from 'typescript'
-import mergeWith from 'lodash.mergewith'
 import {analyze} from './analyze'
-import {calculateHalstead} from './halstead'
 
 async function run(): Promise<void> {
   try {
     const sha = core.getInput('sha')
     const actor = core.getInput('actor')
     const workingDirectory = core.getInput('working_directory')
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const scriptTarget: any = core.getInput('script_target')
     const filename = analyze(sha, actor, workingDirectory, scriptTarget)
 
