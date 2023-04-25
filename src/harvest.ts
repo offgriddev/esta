6fd399febbea0d4f202d22a52c9bae79740eb8b9
Jesse Anderson <jeryanders@gmail.com>
2023-04-25 16:24:15 -0400

    chore: package

--- src/harvest.ts
+++ src/harvest.ts
@@ -1,4 +1,4 @@
-import core from '@actions/core'
+import * as core from '@actions/core'
 import ts from 'typescript'
 import {calculateHalstead} from './halstead'
 import {calculateComplexity} from './complexity'
