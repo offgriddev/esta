b0994ef5e21f07b801416e4c82661e68498ab58c
Jesse Anderson <jeryanders@gmail.com>
2023-04-25 18:27:03 -0400

    chore: package

--- src/__tests__/harvest.test.ts
+++
@@ -1,5 +0,0 @@
-import {analyzeTypeScript} from '../harvest'
-describe(__filename, () => {
-  it('calls tscomplex.calculateCyclomaticComplexity with list of files', () => {})
-  it('returns a list of metrics', () => {})
-})
