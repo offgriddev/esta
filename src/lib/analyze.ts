aef46f2e23c4651a7c84b418ed97d4feff2f4ba8
Jesse Anderson <jeryanders@gmail.com>
2023-04-26 16:41:26 -0400

    feat: allow multiple directories

--- src/lib/analyze.ts
+++ src/lib/analyze.ts
@@ -1,7 +1,8 @@
 import ts from 'typescript'
 import {getSourceFile} from './utils'
 import {analyzeTypeScript} from './harvest'
-import {mkdir, writeFile} from 'fs/promises'
+import { mkdir, writeFile } from 'fs/promises'
+import { existsSync } from 'fs'
 import { logger } from '../cmds/lib/logger'
 
 export async function analyze(
@@ -35,7 +36,9 @@ export async function analyze(
     analysis,
     dateUtc: new Date().toISOString()
   }
-  await mkdir(folder)
+  if (!existsSync(folder)) {
+    await mkdir(folder)
+  }
   await writeFile(filename, JSON.stringify(analytics, undefined, 2))
 
   logger.info(`complexity assessment written: ${filename}`)
