5f3e255be17a975bb09233f2d08dc12555323f30
Jesse Anderson <jeryanders@gmail.com>
2023-04-28 16:04:37 -0400

    ci: test diff

--- src/action/main.ts
+++ src/action/main.ts
@@ -1,6 +1,25 @@
 import * as core from '@actions/core'
 import {analyze} from '../lib/analyze'
 import {GithubContext} from '../lib/types'
+import {context, getOctokit} from '@actions/github'
+
+async function getDiff(token) {
+  if (token && context.payload.pull_request) {
+    const octokit = getOctokit(token)
+
+    const result = await octokit.rest.repos.compareCommits({
+      repo: context.repo.repo,
+      owner: context.repo.owner,
+      head: context.payload.pull_request.head.sha,
+      base: context.payload.pull_request.base.sha,
+      per_page: 100
+    })
+
+    return result.data.files || []
+  }
+
+  return []
+}
 
 async function run(): Promise<void> {
   try {
@@ -12,6 +31,9 @@ async function run(): Promise<void> {
     const scriptTarget: any = core.getInput('ecma_script_target')
     const filename = await analyze(workingDirectory, scriptTarget, github)
 
+    core.info(JSON.stringify(context, undefined, 2))
+    const diff = await getDiff(github.token)
+    core.info(JSON.stringify(diff, undefined, 2))
     // get the files and functions that were modified by the actor
     // get complexity diff
     // get min, max, mean, avg for halstead metrics
